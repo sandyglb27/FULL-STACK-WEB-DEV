a5a2f902c21bc9563c51da35cecf17cb96a72669
sandy <sandeepsharan15@gmail.com>
2021-03-13 00:48:14 +0530

    first commit

---
+++ Lec8.js
@@ -0,0 +1,10 @@
+console.log('hello console');
+console.log(4+5);
+console.log(7*5);
+console.table({sandy:'welcome',marks: 90})
+console.warn('this is warning');
+// variable in JS
+// var,let ,const
+ 
+
+ 
