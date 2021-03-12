60be1eb43ef56c4575047aabe750067bc9097de3
sandy <sandeepsharan15@gmail.com>
2021-03-13 01:37:34 +0530

    first commit

--- Lec8.js
+++ Lec8.js
@@ -5,6 +5,47 @@ console.table({sandy:'welcome',marks: 90})
 console.warn('this is warning');
 // variable in JS
 // var,let ,const
+ var name="sandy";
+ console.log(name)
+ var marks=45;
+ console.log(marks)
  
+ // rule for creating javascript variable
+ /*
+ 1. cannot start with numbers
+ 2. can start with letter,numbers,_,or $
+ 3. are case sensitive
+ */
+
+ var city = 'Delhi';
+console.log(city);
+
+const ownersName = 'Hari Ram';
+// ownersName = 'Harry'; // Cannot do this (error)
+console.log(ownersName);
+const fruit = 'Orange';
+
+{
+ let city = 'Rampur';   
+ city = 'Kolkata';
+ console.log(city);
+}
+console.log(city);
+
+
+const arr1 = [12,23,12,53, 3];
+// arr1.push(45);
+console.log(arr1)
+
+/* Most common programming case types:
+
+1. camelCase  
+2. kebab-case
+3. snake_case 
+4. PascalCase 
+
+
+*/
+
 
  
