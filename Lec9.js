392620317e9037c6284e13dc1e8749e909edbf56
sandy <sandeepsharan15@gmail.com>
2021-03-13 01:50:25 +0530

    first commit

---
+++ Lec9.js
@@ -0,0 +1,17 @@
+// primitives data type
+
+// string
+
+var name="sandy";
+console.log(name);
+
+// numbers
+let marks=45;
+console.log('marks');
+
+// undefined
+
+let undef;
+
+// reference data type
+
