1f13a77e9e6c0b391ade9531a27d336fdd1241f3
sandy <sandeepsharan15@gmail.com>
2021-03-15 01:17:50 +0530

    first commit

---
+++ Lec10.js
@@ -0,0 +1,50 @@
+// type conversion and type correction
+   console.log('welcome to Lec10');
+   
+   
+   let myvar;
+   myvar=34;
+   console.log(myvar,(typeof myvar));
+   
+   
+   let booleanvar= String(true);
+    console.log(booleanvar,(typeof booleanvar));
+   
+   
+   
+    let ram = String(ram);
+    console.log(ram, (typeof ram));
+
+    
+    let arr =[1,2,3,4,5,6,7];
+     console.log(arr.length , (typeof arr));
+
+
+     let i=8;
+     console.log(i.toString());
+
+     let stri =Number("345656");
+     console.log(stri, (typeof stri));
+
+
+
+     let Number= parseInt("34");
+     console.log(Number,(typeof Number));
+
+
+     let Number = parseFloat("34.674574");
+     console.log(Number ,(typeof Number));
+
+
+     // type co ercion
+
+     let mystr ="6754";
+     let mynum ="35";
+
+     console.log( mystr+mynum);
+      
+
+
+      
+
+     
---
+++ Lec11.js
@@ -0,0 +1,17 @@
+console.log('we are at Lec 11');
+
+const name = "SANDY";
+const greeting = " good morning";
+console.log(greeting+ ' '+ name );
+
+/*let html; 
+html = html.concat('this','str2');
+console.log(html);
+console.log(html.length);
+console.log(html.tolowercase());
+console.log(html.touppercase());
+*/
+
+// console.log(html.indexof('this'));
+
+
