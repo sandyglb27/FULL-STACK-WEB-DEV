3ae91a5c714c2a0fcde3603314519ac8cfe8a73c
sandy <sandeepsharan15@gmail.com>
2021-03-15 01:17:28 +0530

    first commit

---
+++ Lec12.js
@@ -0,0 +1,17 @@
+console.log('lets discuss abouth array');
+
+const marks = [24, 25, 23, 55,78];
+const fruits = [ 'orange', 'apple', 'pineapple', 'banana'];
+const mixed = ['str',89, [3,5]];
+ 
+
+const arr = new Array(45,68,29,'orange');
+console.log(arr);
+console.log(mixed);
+console.log(fruits[2]);
+
+console.log(arr.length);
+console.log(Array.isArray('ghdsghd'));
+
+
+ 
