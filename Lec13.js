88315202109e37a7f90061e7d6899177d45f6d82
sandy <sandeepsharan15@gmail.com>
2021-03-20 02:05:04 +0530

    first commit

---
+++ Lec13.js
@@ -0,0 +1,35 @@
+console.log('this is lec 13');
+
+
+/*const age = 19;
+
+if(age){
+    console.log('age is 19')
+}
+else
+{
+    console.log('age is not 19')
+}
+*/
+
+/*const age = 29;
+
+if(age){
+    console.log('age is 19')
+}
+else
+{
+    console.log('age is not 19')
+}
+*/
+
+const age = 29;
+
+if(age){
+    console.log('age is 19')
+}
+else
+{
+    console.log('age is not 19')
+}
+
---
+++ Lec14.js
@@ -0,0 +1,21 @@
+console.log('this is Lec14');
+
+let a = window;
+
+//window.alert('hello sandy');
+
+//a= prompt('this will destroy your computer');
+
+//a= confirm('are you sure you want to delete this page');
+
+a= window.innerHeight;
+a= window.innerWidth;
+a= scrollX;
+a= scrollY;
+a= location;
+a= window.history;
+
+
+
+
+console.log(a);
