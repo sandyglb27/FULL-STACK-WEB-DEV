113d06c2547cd30c83abec42fc0569ac02c75e85
sandy <sandeepsharan15@gmail.com>
2021-04-26 02:29:20 +0530

    first commit

---
+++ Lec17.js
@@ -0,0 +1,22 @@
+// uou have to create variable which is a string containing the text which is a link you are interested in.
+
+// you have to fetch all the links from a given page which contain text.
+
+//codewithharry.com
+//javascript
+
+console.log('welcome to tutorial 17');
+/*
+ element selectors:
+ 1.single element selector
+ 2.multi element selector
+
+ */
+
+ let element = document.getElementById('myfirst');
+ element= element.className;
+ //element= element.childNodes;
+ //element= element.parentNode;
+ //element.style.color= 'red';
+ console.log(element);
+  
