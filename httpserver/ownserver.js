149c6cc5eb9be985bddad05e111c4fe7def63076
sandy <sandeepsharan15@gmail.com>
2021-06-23 23:52:35 +0530

    first commit

---
+++ httpserver/ownserver.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req,res) => {
+
+  res.end("hello from this side");
+
+
+});
+
+
+server.listen(8000, "127.0.0.1" , () =>{
+    console.log("listening to the port no 8000");
+
+)};
+
